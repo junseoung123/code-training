897e6ff045fdf1312e8395557fc1b5651a5cb6d7
Junseoung Lee <102960061+junseoung123@users.noreply.github.com>
2022-11-21 00:05:24 +0900

    함수

---
+++ stage05/NO15596.java
@@ -0,0 +1,30 @@
+package stage05;
+
+import java.util.Scanner;
+
+public class NO15596 {
+
+	public static void main(String[] args) {
+		// 15596
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		long[] a = new long[n];
+		long sum = 0;
+		
+		for(int i=0; i<a.length; i++) {
+			a[i] = sc.nextInt();
+			
+		}
+		for(int i=0; i<a.length; i++) {
+			sum += a[i];
+			
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
